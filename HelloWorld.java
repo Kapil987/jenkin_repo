b6ade5cd74f394b7d88ef39bbf4a88ebbf223645
vinod kumar <vinodharyana8@gmail.com>
2021-01-29 16:25:02 +0530

    added bug to print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi, Complete, Good Evening"); 
+        System.out.println("Hi, bug, Good Evening"); 
     }
 }
 
